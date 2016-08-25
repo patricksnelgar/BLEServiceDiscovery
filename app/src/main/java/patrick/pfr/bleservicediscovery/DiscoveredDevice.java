f7f3080b46427fa3e518430e69c0a3cc1d3965a2
Patrick <patrick_snelgar@live.com>
2016-08-25 16:26:13 +1200

    25/8 Sync

---
+++ app/src/main/java/patrick/pfr/bleservicediscovery/DiscoveredDevice.java
@@ -0,0 +1,39 @@
+package patrick.pfr.bleservicediscovery;
+
+import android.bluetooth.BluetoothDevice;
+
+/**
+ * Created by Patrick on 27/01/2016.
+ */
+public class DiscoveredDevice {
+
+    private static final String TAG = DiscoveredDevice.class.getSimpleName();
+
+    public String name;
+    public boolean bondedDevice;
+    public String icon;
+    public String address;
+    public BluetoothDevice btDevice;
+
+    public DiscoveredDevice(String name, boolean bonded, String icon, String address, BluetoothDevice device){
+        this.name = name;
+        this.bondedDevice = bonded;
+        this.icon = icon;
+        this.address = address;
+        this.btDevice = device;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof DiscoveredDevice){
+            DiscoveredDevice a = (DiscoveredDevice) o;
+            if(this.address.equals(a.address)) {
+                //Log.i(TAG, "Address match");
+                //MAC addresses are unique (unless spoofed) so able to be used as identifier.
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ app/src/main/java/patrick/pfr/bleservicediscovery/Service.java
@@ -0,0 +1,7 @@
+package patrick.pfr.bleservicediscovery;
+
+/**
+ * Created by hrtwps on 22/08/2016.
+ */
+public class Service {
+}
